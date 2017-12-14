02a284190e2c922bbf23ef90953d5cc8f0258d22
Jason Vance <jbvance@gmail.com>
2017-12-14 20:26:06 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,9 @@ class PostsController < ApplicationController
 	  redirect_to post_path(@post)
 	end
 
-	 @post = Post.find(params[:id])
+	def edit
+		 @post = Post.find(params[:id])
+	end
 
 	def update
 	end
